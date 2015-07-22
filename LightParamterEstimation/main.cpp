9432f65849767c60b68f48cb0fdbe96b9fe3f6af
Pratheba Selvaraju <prathebaselvaraju@MacBook-Pro.local>
2015-07-21 20:13:19 -0700

    Initial Commit

---
+++ LightParamterEstimation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LightParamterEstimation
+//
+//  Created by Pratheba Selvaraju on 7/21/15.
+//  Copyright (c) 2015 Pratheba Selvaraju. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
